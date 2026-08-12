eb5f603176d53ca4167db64a596242ef11739bf5
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-12 13:17:48 +0700

    feat(designer): open template gallery first

--- apps/designer/lib/main.dart
+++ apps/designer/lib/main.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/material.dart';
-import 'pages/designer_page.dart';
+
+import 'pages/template_gallery_page.dart';
 
 void main() {
   runApp(const DesignerApp());
@@ -8,12 +8,17 @@ void main() {
 
 class DesignerApp extends StatelessWidget {
   const DesignerApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Report Designer - Web/Desktop/Mobile',
-      theme: ThemeData(useMaterial3: true, colorSchemeSeed: Colors.indigo, brightness: Brightness.light),
-      home: const DesignerPage(),
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.indigo,
+        brightness: Brightness.light,
+      ),
+      home: const TemplateGalleryPage(),
       debugShowCheckedModeBanner: false,
     );
   }
