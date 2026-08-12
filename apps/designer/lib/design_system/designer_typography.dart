077c28d192336a8bbf45ad8c78d43576cd29aed8
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-12 21:00:52 +0700

    fix: avoid unbundled Inter dependency

--- apps/designer/lib/design_system/designer_typography.dart
+++ apps/designer/lib/design_system/designer_typography.dart
@@ -3,10 +3,7 @@ import 'package:flutter/material.dart';
 import 'designer_colors.dart';
 
 abstract final class DesignerTypography {
-  static const _family = 'Inter';
-
   static const appTitle = TextStyle(
-    fontFamily: _family,
     fontSize: 16,
     fontWeight: FontWeight.w600,
     height: 1.5,
@@ -14,7 +11,6 @@ abstract final class DesignerTypography {
   );
 
   static const screenTitle = TextStyle(
-    fontFamily: _family,
     fontSize: 18,
     fontWeight: FontWeight.w600,
     height: 28 / 18,
@@ -22,7 +18,6 @@ abstract final class DesignerTypography {
   );
 
   static const panelTitle = TextStyle(
-    fontFamily: _family,
     fontSize: 13,
     fontWeight: FontWeight.w600,
     height: 20 / 13,
@@ -30,7 +25,6 @@ abstract final class DesignerTypography {
   );
 
   static const sectionTitle = TextStyle(
-    fontFamily: _family,
     fontSize: 11,
     fontWeight: FontWeight.w600,
     height: 16 / 11,
@@ -39,7 +33,6 @@ abstract final class DesignerTypography {
   );
 
   static const controlLabel = TextStyle(
-    fontFamily: _family,
     fontSize: 12,
     fontWeight: FontWeight.w500,
     height: 16 / 12,
@@ -47,7 +40,6 @@ abstract final class DesignerTypography {
   );
 
   static const controlValue = TextStyle(
-    fontFamily: _family,
     fontSize: 13,
     fontWeight: FontWeight.w400,
     height: 20 / 13,
@@ -55,7 +47,6 @@ abstract final class DesignerTypography {
   );
 
   static const body = TextStyle(
-    fontFamily: _family,
     fontSize: 13,
     fontWeight: FontWeight.w400,
     height: 20 / 13,
@@ -63,7 +54,6 @@ abstract final class DesignerTypography {
   );
 
   static const helper = TextStyle(
-    fontFamily: _family,
     fontSize: 11,
     fontWeight: FontWeight.w400,
     height: 16 / 11,
@@ -71,7 +61,6 @@ abstract final class DesignerTypography {
   );
 
   static const status = TextStyle(
-    fontFamily: _family,
     fontSize: 11,
     fontWeight: FontWeight.w400,
     height: 16 / 11,
