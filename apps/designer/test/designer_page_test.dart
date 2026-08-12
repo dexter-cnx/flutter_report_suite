4bf0cdd11d274f32ba831e41ec456c333156d639
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-12 10:01:35 +0700

    test: add designer widget suite

---
+++ apps/designer/test/designer_page_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:report_designer/pages/designer_page.dart';
+
+void main() {
+  Widget buildSubject() => const MaterialApp(home: DesignerPage());
+
+  testWidgets('boots the designer with the main authoring controls', (tester) async {
+    await tester.pumpWidget(buildSubject());
+
+    expect(find.text('Report Designer'), findsOneWidget);
+    expect(find.text('Add Element'), findsOneWidget);
+    expect(find.text('Text'), findsOneWidget);
+    expect(find.text('Dynamic {{field}}'), findsOneWidget);
+    expect(find.text('Table {{items}}'), findsOneWidget);
+    expect(find.byTooltip('Preview PDF'), findsOneWidget);
+    expect(find.byTooltip('Export JSON'), findsOneWidget);
+  });
+
+  testWidgets('adding text selects it and exposes editable properties', (tester) async {
+    await tester.pumpWidget(buildSubject());
+
+    await tester.tap(find.text('Text'));
+    await tester.pump();
+
+    expect(find.text('Sample text'), findsOneWidget);
+    expect(find.text('Properties'), findsOneWidget);
+    expect(find.text('Key / Text'), findsOneWidget);
+    expect(find.text('Bold'), findsOneWidget);
+  });
+
+  testWidgets('table elements use the items expression by default', (tester) async {
+    await tester.pumpWidget(buildSubject());
+
+    await tester.tap(find.text('Table {{items}}'));
+    await tester.pump();
+
+    expect(find.text('{{items}}'), findsOneWidget);
+    expect(find.text('Properties'), findsOneWidget);
+  });
+
+  testWidgets('export dialog contains the current template JSON', (tester) async {
+    await tester.pumpWidget(buildSubject());
+
+    await tester.tap(find.text('Dynamic {{field}}'));
+    await tester.pump();
+    await tester.tap(find.byTooltip('Export JSON'));
+    await tester.pumpAndSettle();
+
+    expect(find.text('Template JSON'), findsOneWidget);
+    expect(find.textContaining('"type": "dynamic_text"'), findsOneWidget);
+    expect(find.textContaining('{{shop.name}}'), findsOneWidget);
+  });
+}
