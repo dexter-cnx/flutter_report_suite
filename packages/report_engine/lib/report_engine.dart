2696630ae970e8dae44171002e24eac7ee432e96
dexter-cnx <73522389+dexter-cnx@users.noreply.github.com>
2026-08-12 14:17:52 +0700

    feat(escpos): export printer rendering abstractions

--- packages/report_engine/lib/report_engine.dart
+++ packages/report_engine/lib/report_engine.dart
@@ -6,6 +6,8 @@ export 'src/printer/encoding/esc_pos_text_encoder.dart';
 export 'src/printer/encoding/thai_encoding.dart';
 export 'src/printer/esc_pos_printer_service.dart';
 export 'src/printer/printer_service.dart';
+export 'src/printer/rendering/esc_pos_rasterizer.dart';
+export 'src/printer/rendering/esc_pos_renderer.dart';
 export 'src/printer/transport/esc_pos_transport.dart';
 export 'src/services/pdf_render_service.dart';
 export 'src/services/report_value_resolver.dart';
